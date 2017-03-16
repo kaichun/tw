3f7aa1a351f5ae5abfc9699484d1e75509e7ce1b
kaichun <50533292@qq.com>
2017-03-16 20:02:29 +0800

    Update tw.sh

--- tw.sh
+++ tw.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p /usr/tw
 cd /usr/tw/
-wget https://raw.githubusercontent.com/kaichun/devopstw/master/docker-compose.yml --no-check-certificate
+wget https://raw.githubusercontent.com/kaichun/tw/master/docker-compose.yml --no-check-certificate
 
 docker-compose up -d
